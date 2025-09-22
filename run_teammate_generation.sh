3201aa1e1d042af209e363cee5a9ed08918a33a0
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2025-09-22 22:45:03 +0200

    Chore: Quick script fix

--- run_teammate_generation.sh
+++ run_teammate_generation.sh
@@ -1,8 +1,11 @@
 DEFAULTVALUE=0
 device="${1:-$DEFAULTVALUE}"
-dfficulty="${2:-"medium"}"
+dfficulty="${2:-"easy"}"
+# Are exclusive. String will always overwrite int if both given and name is not empty.
 layout_idx="${3:-0}"
+layout_name="${4:-""}"
 
 CUDA_VISIBLE_DEVICES=${device} LD_LIBRARY_PATH="" nice -n 5 python -m eval_agents_generation.run \
     --layout_difficulty ${dfficulty} \
-    --layout_idx ${layout_idx}
+    --layout_idx ${layout_idx} \
+    --layout_name ${layout_name}
